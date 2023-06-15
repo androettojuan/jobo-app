9267e2055a70c5256668f9362db7d2a041b1a937
Juan Manuel Androetto <juan.androetto36@gmail.com>
2023-06-15 15:11:30 -0300

    jobo app

---
+++ src/pages/routes.js
@@ -0,0 +1,13 @@
+import AccountPage from "./AccountPage/AccountPage";
+import HomePage from "./HomePage/HomePage";
+
+export const routes = [
+  {
+    path: "/",
+    component: HomePage,
+  },
+  {
+    path: "/account",
+    component: AccountPage,
+  },
+];
